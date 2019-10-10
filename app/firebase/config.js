53a8a8fb01c914c28a7b09400ffd9182eda7d433
gd <wjdrlehs57@gmail.com>
2019-10-10 23:23:09 +0900

    firebase init function

---
+++ app/firebase/config.js
@@ -0,0 +1,44 @@
+// import { firebaseJSFileLoad } from '../util/utils';
+
+const firebaseJSFileLoad = firebaseInitFunc => {
+    const scripts = [
+        'https://www.gstatic.com/firebasejs/7.1.0/firebase-app.js',
+        'https://www.gstatic.com/firebasejs/7.1.0/firebase-storage.js',
+        'https://www.gstatic.com/firebasejs/7.1.0/firebase-firestore.js',
+        'https://www.gstatic.com/firebasejs/7.1.0/firebase-auth.js',
+    ];
+    
+    let count = 0;
+    const recursivelyAddScript = (script, cb) => {
+      const el = document.createElement('script')
+      el.src = script;
+      if(count < scripts.length) {
+        count ++
+        el.onload = () => {
+            recursivelyAddScript(scripts[count]);
+        };
+        document.head.appendChild(el);
+      } else {
+        console.log('All script loaded')
+        firebaseInitFunc();
+        return
+      }
+    }
+     
+    recursivelyAddScript(scripts[count]);
+};
+  
+firebaseJSFileLoad(() => {
+    // Your web app's Firebase configuration
+    const firebaseConfig = {
+        apiKey: 'AIzaSyCIIZXVFNo0rktUqj7aadfIAMDY5kzB2gU',
+        authDomain: 'ziium-bb222.firebaseapp.com',
+        databaseURL: 'https://ziium-bb222.firebaseio.com',
+        projectId: 'ziium-bb222',
+        storageBucket: 'ziium-bb222.appspot.com',
+        messagingSenderId: '361643278079',
+    };
+
+    // Initialize Firebase
+    window.fb = firebase.initializeApp(firebaseConfig);
+});
