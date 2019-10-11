647a983282cb023bf87cb0669360b61aa896c54b
gd <wjdrlehs57@gmail.com>
2019-10-11 22:18:48 +0900

    login 함수 구현

--- app/js/login.js
+++ app/js/login.js
@@ -6,9 +6,12 @@ var login_data = {
 $(function() {
     $("#login_status").hide();
 
-    $("#click_login").on("click", function() {
+    $("#click_login").on("click", async function() {
         var id_value = $("#id_val").val();
         var pw_value = $("#pw_val").val();
+        // test id : test@gmail.com
+        // test pw : testtt
+        console.log('emailLogin result', await emailLogin(id_value, pw_value));
 
         if(login_data.id == id_value && login_data.password == pw_value) {
             sessionStorage.setItem("Token", "1");
