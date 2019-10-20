737bda3291a8a70ba378a76cebd7da4d642a46ed
gd <wjdrlehs57@gmail.com>
2019-10-20 21:05:55 +0900

    이미지 업로드 가능한지 테스트

--- app/firebase/houseItem.js
+++ app/firebase/houseItem.js
@@ -1,13 +1,35 @@
-const wirteHouseItem = (userId, name, email, imageUrl) => {
-    db.ref('users/' + userId).set({
-      username: name,
-      email: email,
-      profile_picture : imageUrl
-    }, error => {
-        if (error) {
-            console.log('wirteHouseItem error', error);
-        } else {
-            console.log('wirteHouseItem success');
-        }
+// 물건을 저장하는 기준
+// 도 -> 시 -> 구 -> 동으로 폴더 기준하여 저장한다.
+// 저장하는 값을 입력하는 페이지의 기준으로 작성한다.
+
+const writeHouseItem = async (userId, itemInfo) => {
+    await dbItemWrite({
+        username: userId,
+        ...itemInfo,
     });
+
+    await storageItemWrite(itemInfo.imageInfo);
 };
+
+
+const dbItemWrite = itemInfo => {
+    // db.ref()
+    db.ref('test').set({
+        username: 'test',
+        imageName : 'test/png',
+        // 넘겨 받는 정보 그대로 넣자.
+        ...itemInfo,
+      }, error => {
+          if (error) {
+              console.log('wirteHouseItem error', error);
+          } else {
+              console.log('wirteHouseItem success');
+          }
+      });
+}
+
+const storageItemWrite = imageInfo => {
+    storage.child('test/' + imageInfo.name).put(imageInfo).then(snapshot => {
+        console.log('suc');
+    });
+}
--- app/js/add_info.js
+++ app/js/add_info.js
@@ -7,6 +7,11 @@ $(function() {
       var img_element = $("<img class='img' style='width: 50px; height: 50px;'>");
       $(img_element).attr("src", event.target.result);
       $("#img_list").append(img_element);
+      
+      // Upload the file and metadata
+      // storage.child('test/' + files.name).put(files).then(snapshot => {
+      //   console.log('suc');
+      // });
     }
 
     reader.readAsDataURL(files);
