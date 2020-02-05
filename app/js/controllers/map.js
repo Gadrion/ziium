9caa5ff26538ebb2c42be47aa787068bc64878c5
shin.sg <tjdrbs588.smg@ctf.ef.hanwha.com>
2020-02-05 22:13:57 +0900

    위치 검색 시 맵 Focus 이동 로직 구현 및 FirebaseService 데이터 쓰기, 가져오기 로직 추가

--- app/js/controllers/map.js
+++ app/js/controllers/map.js
@@ -8,7 +8,7 @@
     5. 동시에 구글맵 클릭 시 해당 좌표에 Marker 생성 후 클릭 event 적용 (매물 생성 팝업)
 */
 
-kindFramework.controller("MapCtrl", function($scope, $location, $q, $compile, TempStorage) {
+kindFramework.controller("MapCtrl", function($scope, $location, $q, $compile, TempStorage, FirebaseService) {
     var map = null;
     var marker_count = 0;
     var marker_content = '<div class="marker_popup">';
@@ -20,30 +20,32 @@ kindFramework.controller("MapCtrl", function($scope, $location, $q, $compile, Te
         marker_content += '</div>';
     marker_content += '</div>';
 
+    var apiKey = "AIzaSyCcETJafZiUGqDHxV0YwS4YAnehfizoLF0";
+
 
     var marker_array = [];
+
     $scope.object_status = false;
+    $scope.searchAddressData = '';
 
-    function initMap () {  // SessionStorage에서 Login 토큰 있을 때 GoogleMap Api 실행
-        var deffered = $q.defer();
+    function initMap (location) {  // SessionStorage에서 Login 토큰 있을 때 GoogleMap Api 실행
+        var deferred = $q.defer();
 
-        var X_point = 128.558612;       // X 좌표
-        var Y_point = 35.837143;        // Y 좌표
+        FirebaseService.getItem().then(function(response) {
+            map = new google.maps.Map(document.getElementById("map_ma"), {
+                center: location,
+                zoom: 16
+            });
 
-        map = new google.maps.Map(document.getElementById("map_ma"), {
-            center: {lat: 35.837143, lng: 128.558612},
-            zoom: 16
-        });
+            var LoginToken = sessionStorage.getItem('Token');
 
-        var LoginToken = sessionStorage.getItem('Token');
-
-        if(LoginToken == 1) {
-            deffered.reject()
-        } else {
-            deffered.resolve();
-        }
-
-        return deffered.promise;
+            if(LoginToken == 1) {
+                deferred.reject()
+            } else {
+                deferred.resolve(response);
+            }
+        })
+        return deferred.promise;
     }
 
     function make_marker (location) {
@@ -94,66 +96,82 @@ kindFramework.controller("MapCtrl", function($scope, $location, $q, $compile, Te
     }
 
     function initMarker(response) {
-        var response = {
-            data: [
-                {
-                    location: {lat: 35.836500, lng: 128.558690},
-                    address: "대구광역시 무슨시 무슨동 60",
-                    description: "상세설명이 필요합니다~"
-                },
-                {
-                    location: {lat: 35.838800, lng: 128.558890},
-                    address: "대구광역시 이런시 저런동 90",
-                    description: "이런~"
-                }
-            ]
-        }
+        // 현재 initMarker는 동작하지 않습니다. 데이터 구성 정리중
 
-        $.each(response.data, function(index, value) {
-            var marker = new google.maps.Marker({
-                position: value.location,
-                map: map
-            });
+        // var response = {
+        //     data: [
+        //         {
+        //             location: {lat: 35.836500, lng: 128.558690},
+        //             address: "대구광역시 무슨시 무슨동 60",
+        //             description: "상세설명이 필요합니다~"
+        //         },
+        //         {
+        //             location: {lat: 35.838800, lng: 128.558890},
+        //             address: "대구광역시 이런시 저런동 90",
+        //             description: "이런~"
+        //         }
+        //     ]
+        // }
 
-            var modify_content = "";
+        // $.each(response.data, function(index, value) {
+        //     var marker = new google.maps.Marker({
+        //         position: value.location,
+        //         map: map
+        //     });
 
-            modify_content = marker_content.replace("%index%", index);
-            modify_content = modify_content.replace("%address%", value.address);
-            modify_content = modify_content.replace("%description%", value.description);
+        //     var modify_content = "";
 
-            var angular_compile_data = $compile(modify_content)($scope);
+        //     modify_content = marker_content.replace("%index%", index);
+        //     modify_content = modify_content.replace("%address%", value.address);
+        //     modify_content = modify_content.replace("%description%", value.description);
 
-            marker.addListener('click', function () {
-                var infoWindow = new google.maps.InfoWindow({
-                    content: angular_compile_data[0]
-                });
+        //     var angular_compile_data = $compile(modify_content)($scope);
+
+        //     marker.addListener('click', function () {
+        //         var infoWindow = new google.maps.InfoWindow({
+        //             content: angular_compile_data[0]
+        //         });
         
-                infoWindow.open(map, marker);
-            })
+        //         infoWindow.open(map, marker);
+        //     })
 
-            marker_array.push({
-                marker: marker,
-                data: value
-            });
-            marker_count = index + 1;
+        //     marker_array.push({
+        //         marker: marker,
+        //         data: value
+        //     });
+        //     marker_count = index + 1;
 
-        })
+        // })
     }
 
     function getAddressInfo(latlng) {
-        var deffered = $q.defer();
-
-        var apiKey = "AIzaSyCcETJafZiUGqDHxV0YwS4YAnehfizoLF0";
+        var deferred = $q.defer();        
         var lat = latlng.lat();
         var lng = latlng.lng();
         var url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + lat + "," + lng + "&key=" + apiKey;
 
         $.get(url, {}, function(data) {
-            deffered.resolve(data);
+            deferred.resolve(data);
         })
 
-        return deffered.promise;
-        
+        return deferred.promise;
+    }
+
+    function getLatLngInfo(address) {
+        /*
+            address 값은 배열로 존재해야 함. ex) ["경기도", "용인시", "수지구", "풍덕천동"]...
+            google geocode에 접근하기 위해서는 띄어쓰기에 +가 적용된 주소값이 필요함. ex) 경기도+용인시+수지구+풍덕천동
+            때문에 배열로 가져온 address 값을 join("+") 를 사용하여 geocode에 접근할 수 있는 데이터를 만들어냄.
+        */
+        var deferred = $q.defer();
+        var addressData = address.join("+");
+        var url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + addressData + "&key=" + apiKey;
+
+        $.get(url, {}, function(data) {
+            deferred.resolve(data);
+        })
+
+        return deferred.promise;
     }
 
     $scope.remove_marker = function (element) {
@@ -175,22 +193,47 @@ kindFramework.controller("MapCtrl", function($scope, $location, $q, $compile, Te
             height: winHeight
         });
 
-        initMap().then(function(response) {
+        initMap({lng: 128.558612, lat: 35.837143}).then(function(response) {
             initMarker(response);
             
             google.maps.event.addListener(map,'click',function(event) {
                 make_marker(event.latLng);
-
-                var x = event.latLng.lat();
-                var y = event.latLng.lng();
             });
         }, function() {
             // location.replace('/views/login.html')
         });
+    });
 
-        $scope.add_object = function() {
-            $scope.object_status = !$scope.object_status;
+    $scope.add_object = function() {
+        $scope.object_status = !$scope.object_status;
+    }
+
+    $scope.searchPosition = function() {
+        // 위치 검색 시 실행되는 함수.
+
+        if($scope.searchAddressData === '') {
+            return;
         }
-    })
+
+        var search = $scope.searchAddressData.split(" ");
+
+        getLatLngInfo(search).then(function(response) {
+            /*
+                맵 Focus를 이동시키기 위해 getLatLngInfo 함수를 사용하여 검색한 주소값의 좌표값을 가져온 후
+                initMap() 함수에 파라메터 값만 수정하여 map을 재 구성.
+                initMarker() 함수는 추가로 실행되면 Marker 데이터가 무수히 많이 쌓이는 버그가 발생하므로,
+                initMap 실행 완료 후 initMarker()를 추가적으로 실행시키지 않아야 함.
+                단, click 이벤트는 다시 활성화시켜줘야 물건추가 버튼을 동작시킬 수 있음.
+            */
+            var location = response.results[0].geometry.location;
+            initMap(location).then(function(response) {
+                google.maps.event.addListener(map,'click',function(event) {
+                    make_marker(event.latLng);
+                });
+            }, function() {
+                // location.replace('/views/login.html')
+            });
+        });
+    }
 });
 
--- app/js/services/firebaseService.js
+++ app/js/services/firebaseService.js
@@ -17,30 +17,83 @@ kindFramework.factory('FirebaseService', function($q) {
   }
 
   FirebaseService.write = function(item) {
-    var addressList = itemInfo.mapData.address.split(' ');
+    var deferred = $q.defer();
+    var addressList = item.mapData.address.split(' ');
     var tempObject = '';
     var addressListLength = addressList.length - 1; // 끝에 숫자로 된 주소를 빼기 위함
-    for(var i = 1; i < addressListLength; i++) {
-        tempObject += `${addressList[i]}/`;
+    for(let i = 1; i < addressListLength; i++) {
+        tempObject += addressList[i] + '/';
     }
 
-    console.log(tempObject);
+    // File 데이터가 있을 때는 postFile 실행 후 db update를 실행
+    // File 데이터가 없을 때는 바로 db update 실행
+
+    if (item.file) {
+        FirebaseService.postFile(item.file).then(function() {
+          db.ref('map/' + tempObject).update({
+            username: 'test',
+            title: item.title,
+            status: item.status,
+            memo: item.memo,
+            option: item.option,
+            file: item.file,
+            mapData: item.mapData
+            }, error => {
+              if (error) {
+                console.log('wirteHouseItem error', error);
+                deferred.reject(false);
+              } else {
+                console.log('wirteHouseItem success');
+                deferred.resolve(true);
+              }
+            }
+          );
+        });
+    } else {
+      db.ref('map/' + tempObject).update({
+        username: 'test',
+        title: item.title,
+        status: item.status,
+        memo: item.memo,
+        option: item.option,
+        file: item.file,
+        mapData: item.mapData
+        }, error => {
+          if (error) {
+            console.log('wirteHouseItem error', error);
+            deferred.reject(false);
+          } else {
+            console.log('wirteHouseItem success');
+            deferred.resolve(true);
+          }
+        }
+      );
+    }
+
+    return deferred.promise;
   }
 
+  FirebaseService.postFile = function(file) {
+    var deferred = $q.defer();
+
+    storage.child('test/' + file.name).put(file).then(function(snapshot) {
+        console.log('suc', snapshot);
+        deferred.resolve(snapshot);
+    });
+
+    return deferred.promise;
+  }
+
+  FirebaseService.getItem = function() {
+    var deferred = $q.defer();
+
+    db.ref('map').once('value', function(data) {
+      deferred.resolve(data.val());
+      console.log('data value', data.val());
+    });
+
+    return deferred.promise;
+  }
 
   return FirebaseService;
 })
-
-/*
-
-function EmailLogin(email, password) {
-  firebase.auth().signInWithEmailAndPassword(email, password).then(function () {
-    console.log('succes');
-    return true;
-  }, function(error) {
-    console.log('emailLogin error', error);
-    return false;
-  })
-}
-
-*/
